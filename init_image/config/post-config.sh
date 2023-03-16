e35fd19826e62ad3e64b5c99d0c9b15c232ab50f
Damian Lezcano <lezcano.da@gmail.com>
2023-03-16 14:00:53 -0300

    cambios

--- init_image/config/post-config.sh
+++ init_image/config/post-config.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo "#### Custom config start. #### v2"
+echo "#### Custom config start. #### v3"
 
-
-diverts=""
-diverts="      ${diverts}<cluster-connections>\n"
-diverts="      ${diverts}   <cluster-connection name=\"my-cluster\">\n"
-diverts="      ${diverts}      <connector-ref>artemis</connector-ref>\n"
-diverts="      ${diverts}      <static-connectors>\n"
-diverts="      ${diverts}         <connector-ref>broker1-connector</connector-ref>\n"
-diverts="      ${diverts}         <connector-ref>broker2-connector</connector-ref>\n"
-diverts="      ${diverts}      </static-connectors>\n"
-diverts="      ${diverts}   </cluster-connection>\n"
-diverts="      ${diverts}</cluster-connections>\n\n"
+cc=""
+cc="      ${cc}\n\n<cluster-connections>\n"
+cc="      ${cc}   <cluster-connection name=\"my-cluster\">\n"
+cc="      ${cc}      <connector-ref>artemis</connector-ref>\n"
+cc="      ${cc}      <static-connectors>\n"
+cc="      ${cc}         <connector-ref>broker1-connector</connector-ref>\n"
+cc="      ${cc}         <connector-ref>broker2-connector</connector-ref>\n"
+cc="      ${cc}      </static-connectors>\n"
+cc="      ${cc}   </cluster-connection>\n"
+cc="      ${cc}</cluster-connections>\n\n"
 
 # removemos el tag
 sed -i '/<cluster-connections>/,/<\/cluster-connections>/d' ${CONFIG_INSTANCE_DIR}/etc/broker.xml
 
 # generamos el tag justo arriba de security-settings
-sed -i "s|  <security-settings>|${diverts}  <security-settings> ${address}|g" ${CONFIG_INSTANCE_DIR}/etc/broker.xml
+#sed -i "s|  </security-settings>|${cc}  </security-settings> ${cc}|g" ${CONFIG_INSTANCE_DIR}/etc/broker.xml
+
 
 echo "#### Custom config done. ####"
