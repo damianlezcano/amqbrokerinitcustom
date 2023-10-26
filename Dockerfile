0cdee28b668a5533306df91d548c00733b919242
Damian Lezcano <lezcano.da@gmail.com>
2023-10-26 14:08:43 -0300

    agrego profile mirroring

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM registry.redhat.io/amq7/amq-broker-init-rhel8:7.10-38.1675807822
 
-ADD config /amq/scripts
+# opciones: clustering y mirroring
+ARG PROFILE=clustering
+
+ADD config/post-config-${PROFILE}.sh /amq/scripts/post-config.sh
 
 USER root
 RUN chmod -R 774 /amq/scripts 
---
+++ config/post-config-clustering.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo "#### Custom config start. ####"
+
+cc=""
+cc="      ${cc}\n\n<cluster-connections>\n"
+cc="      ${cc}   <cluster-connection name=\"my-cluster\">\n"
+cc="      ${cc}      <connector-ref>artemis</connector-ref>\n"
+cc="      ${cc}      <retry-interval>500</retry-interval>\n"
+cc="      ${cc}      <use-duplicate-detection>true</use-duplicate-detection>\n"
+cc="      ${cc}      <message-load-balancing>STRICT</message-load-balancing>\n"
+cc="      ${cc}      <max-hops>1</max-hops>\n"
+cc="      ${cc}      <static-connectors>\n"
+cc="      ${cc}         <connector-ref>broker2-connector</connector-ref>\n"
+cc="      ${cc}      </static-connectors>\n"
+cc="      ${cc}   </cluster-connection>\n"
+cc="      ${cc}</cluster-connections>\n\n"
+
+#cc="      ${cc}<broker-connections>\n"
+#cc="      ${cc}  <amqp-connection uri=\"tcp://ex-aao-artemis-0-svc.pocamqbroker1.svc.cluster.local:61616\" name=\"DC1\">\n"
+#cc="      ${cc}    <mirror/>\n"
+#cc="      ${cc}  </amqp-connection>\n"
+#cc="      ${cc}</broker-connections>\n\n"
+
+cc="       ${cc}<addresses>\n"
+cc="       ${cc}   <address name=\"DLQ\">\n"
+cc="       ${cc}      <anycast>\n"
+cc="       ${cc}         <queue name=\"DLQ\" />\n"
+cc="       ${cc}      </anycast>\n"
+cc="       ${cc}   </address>\n"
+cc="       ${cc}   <address name=\"ExpiryQueue\">\n"
+cc="       ${cc}      <anycast>\n"
+cc="       ${cc}         <queue name=\"ExpiryQueue\" />\n"
+cc="       ${cc}      </anycast>\n"
+cc="       ${cc}   </address>\n"
+
+cc="       ${cc}   <address name=\"exampleTopic\">\n"
+cc="       ${cc}      <multicast/>\n"
+cc="       ${cc}   </address>\n"
+
+cc="       ${cc}   <address name=\"exampleQueue\">\n"
+cc="       ${cc}      <anycast>\n"
+cc="       ${cc}         <queue name=\"exampleQueue\"/>\n"
+cc="       ${cc}      </anycast>\n"
+cc="       ${cc}   </address>\n"
+
+cc="       ${cc}   <address name=\"test\">\n"
+cc="       ${cc}      <multicast/>\n"
+cc="       ${cc}   </address>\n"
+cc="       ${cc}   <address name=\"/topic/test\">\n"
+cc="       ${cc}      <multicast/>\n"
+cc="       ${cc}   </address>\n"
+cc="       ${cc}</addresses>\n\n"
+
+cc="       ${cc}<security-enabled>false</security-enabled>\n\n"
+
+sed -i '/<addresses>/,/<\/addresses>/d' ${CONFIG_INSTANCE_DIR}/etc/broker.xml
+
+sed -i '/<cluster-connections>/,/<\/cluster-connections>/d' ${CONFIG_INSTANCE_DIR}/etc/broker.xml
+
+sed -i "s|  </discovery-groups>| </discovery-groups> ${cc} |g" ${CONFIG_INSTANCE_DIR}/etc/broker.xml
+
+echo "#### Custom config done. ####"
