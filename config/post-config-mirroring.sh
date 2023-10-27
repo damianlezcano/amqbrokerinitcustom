d61c5367f505a49881c1bfbf0e521b25d87946ce
Damian Lezcano <lezcano.da@gmail.com>
2023-10-27 18:04:44 -0300

    agrego amq3

--- config/post-config-mirroring.sh
+++ config/post-config-mirroring.sh
@@ -5,7 +5,7 @@ echo "#### Custom config start. ####"
 cc=""
 
 cc="      ${cc}<broker-connections>\n"
-cc="      ${cc}  <amqp-connection uri=\"tcp://ex-aao-artemis-0-svc.pocamqbroker1.svc.cluster.local:61616\" name=\"DC1\">\n"
+cc="      ${cc}  <amqp-connection uri=\"tcp://ex-aao-artemis-0-svc.pocamqbroker3.svc.cluster.local:61616\" name=\"DC1\">\n"
 cc="      ${cc}    <mirror sync=\"true\"/>\n"
 cc="      ${cc}  </amqp-connection>\n"
 cc="      ${cc}</broker-connections>\n\n"
